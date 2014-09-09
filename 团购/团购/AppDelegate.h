f6e23a22dc91b1a0cd98fa6c4980286dee06d1d0
WinPain <807410103@qq.com>
2014-09-09 18:40:12 +0800

    开始

---
+++ 团购/团购/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  团购
+//
+//  Created by 刘志华 on 14-9-9.
+//  Copyright (c) 2014年 刘志华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
