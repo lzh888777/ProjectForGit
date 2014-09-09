8689c3321e1647f61dcd7b6f59b1451fb21edd03
WinPain <807410103@qq.com>
2014-09-09 18:44:24 +0800

    建立DockView

---
+++ 团购/团购/Dock.m
@@ -0,0 +1,31 @@
+//
+//  Dock.m
+//  团购
+//
+//  Created by 刘志华 on 14-9-9.
+//  Copyright (c) 2014年 刘志华. All rights reserved.
+//
+
+#import "Dock.h"
+
+@implementation Dock
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
