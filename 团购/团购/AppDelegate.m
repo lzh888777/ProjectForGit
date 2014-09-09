95644d2ecf47ae03c646f7376db8149b3adbacec
WinPain <807410103@qq.com>
2014-09-09 19:22:55 +0800

    123
    321

--- 团购/团购/AppDelegate.m
+++ 团购/团购/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
@@ -15,6 +16,9 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    MainViewController *con = [[MainViewController alloc]init];
+    self.window.rootViewController = con;
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ 团购/团购/TabBarItemView.m
@@ -0,0 +1,31 @@
+//
+//  TabBarItemView.m
+//  团购
+//
+//  Created by 刘志华 on 14-9-9.
+//  Copyright (c) 2014年 刘志华. All rights reserved.
+//
+
+#import "TabBarItemView.h"
+
+@implementation TabBarItemView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
